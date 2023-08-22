9b5ea81f20b3079810188d5fbc2bd91f5484b1d3
semil338 <semilkhedawala@gmail.com>
2023-08-22 20:34:45 +0530

    productList refactor and use API for data

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,5 +1,29 @@
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+export function fetchProductsByFilters(filter, sort) {
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length > 0) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      `http://localhost:8080/products?${queryString}`
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
 }
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,21 +1,29 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchCount } from "./productAPI";
+import { fetchAllProducts, fetchProductsByFilters } from "./productAPI";
 
 const initialState = {
-  value: 0,
+  products: [],
   status: "idle",
 };
 
-export const incrementAsync = createAsyncThunk(
-  "counter/fetchCount",
-  async (amount) => {
-    const response = await fetchCount(amount);
+export const fetchAllProductsAsync = createAsyncThunk(
+  "product/fetchAllProducts",
+  async () => {
+    const response = await fetchAllProducts();
+    return response.data;
+  }
+);
+
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  "product/fetchProductsByFilters",
+  async ({ filter, sort }) => {
+    const response = await fetchProductsByFilters(filter, sort);
     return response.data;
   }
 );
 
 export const productSlice = createSlice({
-  name: "counter",
+  name: "product",
   initialState,
   reducers: {
     increment: (state) => {
@@ -30,18 +38,25 @@ export const productSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(fetchAllProductsAsync.pending, (state) => {
         state.status = "loading";
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
         state.status = "idle";
-        state.value += action.payload;
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.products = action.payload;
       });
   },
 });
 
 export const { increment, decrement, incrementByAmount } = productSlice.actions;
 
-export const selectCount = (state) => state.counter.value;
+export const selectAllProducts = (state) => state.product.products;
 
 export default productSlice.reducer;
