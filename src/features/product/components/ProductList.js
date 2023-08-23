91a1661954a0ee4111a84cbd87544fac1328a516
Semil Khedawala <71251631+semil338@users.noreply.github.com>
2023-08-23 22:58:30 +0530

    Update ProductList.js

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -594,6 +594,7 @@ function MobileFilter({
   );
 }
 
+
 function Pagination() {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
